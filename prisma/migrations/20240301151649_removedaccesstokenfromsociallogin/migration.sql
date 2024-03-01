c9c9ba31196e98420ca3d636908e508637569cdc
sunnynk19 <sunnynk19@gmail.com>
2024-03-01 20:47:05 +0530

    sync fixing bug fixed

---
+++ prisma/migrations/20240301151649_removedaccesstokenfromsociallogin/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `SocialLogin` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `SocialLogin` DROP COLUMN `accessToken`;
